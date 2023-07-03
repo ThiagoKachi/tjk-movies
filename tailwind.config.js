618bed23c33ac51efcaafe590c0e0007ea7c00e3
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-07-03 20:51:12 -0300

    feat: first commit - Install dependencies and make sidebar

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Raleway', 'sans-serif'],
+      serif: ['Raleway', 'serif'],
+      body: ['Raleway'],
+    },
+  },
+  plugins: [],
+};
+
