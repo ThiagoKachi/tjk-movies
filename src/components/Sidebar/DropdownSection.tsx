7c22994e90028feed42068598cf1f0db90c11b9c
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-07-17 21:05:15 -0300

    feat: create sidebar

--- src/components/Sidebar/DropdownSection.tsx
+++ src/components/Sidebar/DropdownSection.tsx
@@ -24,34 +24,34 @@ export function DropdownSection({
   return (
     <>
       <div
-        className="p-2.5 mt-3 flex items-center rounded-sm px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+        className="p-2 mt-3 flex items-center rounded-sm px-2 duration-300 cursor-pointer hover:bg-gray-100 text-white"
         onClick={toggleDropdown}
       >
-        <i className="text-[20px]">
+        <i className="text-[20px] text-gray-900">
           <BsFilm />
         </i>
         <div className="flex justify-between w-full items-center">
-          <span className="text-[16px] ml-4 text-gray-200 font-bold">
+          <span className="text-[16px] ml-4 text-gray-900 font-medium">
             Gêneros
           </span>
           <span className="text-sm">
-            <i className="text-xl">
+            <i className="text-xl text-gray-900">
               {isDropdownOpen ? <BsChevronUp /> : <BsChevronDown />}
             </i>
           </span>
         </div>
       </div>
       {isDropdownOpen && (
-        <div className="text-left text-md mt-2 pl-2 mx-auto text-gray-200 font-bold w-full cursor-pointer">
+        <div className="text-left text-md mt-2 pl-2 mx-auto text-gray-900 font-medium w-full cursor-pointer">
           {genres.map((genre) => (
             <div
               key={genre}
-              className="px-2 py-2 flex align-middle hover:bg-blue-600 rounded-sm"
+              className="px-0 py-2 flex align-middle hover:bg-gray-100 rounded-sm"
             >
               <div className="mr-3 border border-gray-600 w-6 text-center rounded-sm">
                 {genre.slice(0, 1)}
               </div>
-              <h1 className="hover:bg-blue-600 rounded-sm">{genre}</h1>
+              <h1 className="hover:bg-gray-100 rounded-sm">{genre}</h1>
             </div>
           ))}
         </div>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { SidebarSection } from './Section';
 import { DropdownSection } from './DropdownSection';
-import useWindowWidth from '../../hooks/useWindowWidth';
 import {
   AiOutlineFire,
   AiOutlineSearch,
@@ -9,105 +8,130 @@ import {
   AiOutlineAppstore,
   AiOutlineHeart,
   AiOutlineStar,
-  AiOutlineClose,
-  AiOutlineAlignLeft,
 } from 'react-icons/ai';
 import { BsRocketTakeoff, BsBarChart } from 'react-icons/bs';
 
 export function Sidebar() {
-  const windowWidth = useWindowWidth();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(windowWidth >= 768);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const openSidebar = () => {
-    setIsSidebarOpen(true);
-  };
-
-  const closeSidebar = () => {
-    setIsSidebarOpen(false);
-  };
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
-  useEffect(() => {
-    setIsSidebarOpen(windowWidth >= 768);
-  }, [windowWidth]);
-
   return (
     <>
-      <span className="absolute text-white text-4xl top-5 left-4 cursor-pointer z-1">
-        <button
-          className="p-2 bg-gray-900 rounded-sm text-3xl"
-          onClick={openSidebar}
-        >
-          <AiOutlineAlignLeft />
-        </button>
-      </span>
-      <div
-        className={`sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] z-1 overflow-y-auto text-center bg-gray-900 transition-transform duration-200 transform ${
-          isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
-        }`}
-      >
-        <div className="text-gray-100 text-xl">
-          <div className="p-2.5 mt-1 flex items-center">
-            <i className="text-2xl px-1 py-1 rounded-sm bg-blue-600">
-              <AiOutlineFire />
-            </i>
-            <h1 className="font-bold text-gray-200 text-[18px] ml-3">
-              TJKMovies
-            </h1>
-            <i
-              className="cursor-pointer ml-28 md:hidden"
-              onClick={closeSidebar}
-            >
-              <AiOutlineClose />
-            </i>
+      <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-start">
+              <button
+                data-drawer-target="logo-sidebar"
+                data-drawer-toggle="logo-sidebar"
+                aria-controls="logo-sidebar"
+                type="button"
+                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              >
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                  className="w-6 h-6"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                  ></path>
+                </svg>
+              </button>
+              <div className="pl-2 mt-1 flex items-center">
+                <i className="text-2xl px-1 py-1 rounded-sm bg-white">
+                  <AiOutlineFire />
+                </i>
+                <h1 className="font-bold text-gray-700 text-[18px] ml-3">
+                  TJKMovies
+                </h1>
+              </div>
+            </div>
           </div>
-          <div className="my-4 bg-gray-600 h-[1px]"></div>
         </div>
-        <div className="p-2.5 flex items-center rounded-sm px-4 duration-300 cursor-pointer bg-gray-700 text-white">
-          <i className="text-2xl">
-            <AiOutlineSearch />
-          </i>
-          <input
-            type="text"
-            placeholder="Search"
-            className="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
-          />
+      </nav>
+
+      <aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+          <div className="relative mb-2">
+            <input
+              placeholder="Faça sua busca"
+              className="m-2 w-[95%] p-2 rounded-sm border-2 border-solid border-gray-300 placeholder-gray-400 focus:border-blue-400 focus:outline-none"
+            />
+
+            <span className="text-xl text-gray-400 absolute right-4 top-1/2 transform -translate-y-1/2">
+              <AiOutlineSearch />
+            </span>
+          </div>
+
+          <ul className="space-y-2 font-medium">
+            <li>
+              <SidebarSection title="Home">
+                <div className="text-lg">
+                  <AiOutlineHome />
+                </div>
+              </SidebarSection>
+            </li>
+            <li>
+              <SidebarSection title="Explorar">
+                <div className="text-lg">
+                  <AiOutlineAppstore />
+                </div>
+              </SidebarSection>
+            </li>
+            <li>
+              <SidebarSection title="Favoritos">
+                <div className="text-lg">
+                  <AiOutlineHeart />
+                </div>
+              </SidebarSection>
+            </li>
+
+            <li className="border-t-2 pt-2">
+              <DropdownSection
+                isDropdownOpen={isDropdownOpen}
+                toggleDropdown={toggleDropdown}
+              />
+            </li>
+
+            <li>
+              <SidebarSection title="Populares">
+                <div className="text-lg">
+                  <AiOutlineStar />
+                </div>
+              </SidebarSection>
+            </li>
+
+            <li>
+              <SidebarSection title="Melhores filmes">
+                <div className="text-lg">
+                  <BsBarChart />
+                </div>
+              </SidebarSection>
+            </li>
+
+            <li>
+              <SidebarSection title="Próximos lançamentos">
+                <div className="text-lg">
+                  <BsRocketTakeoff />
+                </div>
+              </SidebarSection>
+            </li>
+          </ul>
         </div>
-        <SidebarSection title="Home">
-          <AiOutlineHome />
-        </SidebarSection>
-
-        <SidebarSection title="Explorar">
-          <AiOutlineAppstore />
-        </SidebarSection>
-
-        <SidebarSection title="Favoritos">
-          <AiOutlineHeart />
-        </SidebarSection>
-
-        <div className="my-4 bg-gray-600 h-[1px]" />
-
-        <DropdownSection
-          toggleDropdown={toggleDropdown}
-          isDropdownOpen={isDropdownOpen}
-        />
-
-        <SidebarSection title="Populares">
-          <AiOutlineStar />
-        </SidebarSection>
-
-        <SidebarSection title="Melhores filmes">
-          <BsBarChart />
-        </SidebarSection>
-
-        <SidebarSection title="Próximos lançamentos">
-          <BsRocketTakeoff />
-        </SidebarSection>
-      </div>
+      </aside>
     </>
   );
 }
